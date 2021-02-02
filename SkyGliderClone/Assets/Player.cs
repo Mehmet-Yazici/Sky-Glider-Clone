419614988b58deaf05cd8c80172539539b0f807c
Mehmet Yazıcı <32649344+Mehmet-Yazici@users.noreply.github.com>
2021-02-03 02:02:03 +0300

    Set up Animator

--- SkyGliderClone/Assets/Player.cs
+++ SkyGliderClone/Assets/Player.cs
@@ -6,19 +6,21 @@ public class Player : MonoBehaviour
 {
 
     Rigidbody rb;
+    Animator anim;
     public float moveSpeed;
     Vector3 throwVec; 
    
 
     private void Awake()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
         throwVec = new Vector3(0f, moveSpeed, 0f);
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim.enabled = false;
     }
 
     // Update is called once per frame
@@ -27,6 +29,8 @@ public class Player : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
+            anim.enabled = true;
+            anim.SetBool("isGliding", true);
             rb.AddForce(rb.transform.forward * moveSpeed);
         }   
     }

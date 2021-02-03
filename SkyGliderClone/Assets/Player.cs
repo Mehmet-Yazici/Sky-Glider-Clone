4a8d4281d87b7e4d50bdd77ebb80ad3c7494e7b7
Mehmet Yazıcı <32649344+Mehmet-Yazici@users.noreply.github.com>
2021-02-03 16:34:12 +0300

    throwing player logic added

--- SkyGliderClone/Assets/Player.cs
+++ SkyGliderClone/Assets/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,7 +10,9 @@ public class Player : MonoBehaviour
     Animator anim;
     Transform playerTransform;
     public float moveSpeed;
-    Vector3 throwVec; 
+    Vector3 throwVec;
+    bool onStick = true;
+    
    
 
     private void Awake()
@@ -18,27 +21,43 @@ public class Player : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         playerTransform = GetComponent<Transform>();
         throwVec = new Vector3(0f, moveSpeed, 0f);
+        
     }
     // Start is called before the first frame update
     void Start()
     {
         rb.useGravity = false;
         anim.enabled = false;
+        
     }
+
 
     // Update is called once per frame
     void Update()
     {
-
-        playerTransform.position  = top_bone.position;
-        playerTransform.rotation = top_bone.rotation;
-        /*if (Input.GetMouseButtonDown(1))
+        //on the stick
+        if (onStick)
         {
-            anim.enabled = true;
-            anim.SetBool("isGliding", true);
-            rb.AddForce(rb.transform.forward * moveSpeed);
-        }   */
+            playerTransform.position = top_bone.position;
+            playerTransform.rotation = top_bone.rotation;
+        }
+        
+        if (Input.GetMouseButtonDown(1))
+        {
+            Invoke("ThrowBall",0.31f);
+        }   
+    }
+
+    void ThrowBall()
+    {
+        onStick = false;
+        rb.useGravity = true;
+        anim.enabled = true;
+        anim.SetBool("isGliding", true);
+        rb.AddForce(rb.transform.forward * moveSpeed * 100f);
     }
 
     
+
+
 }

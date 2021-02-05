894e832f94a0771164483d1379308cf1405abd9f
Mehmet Yazıcı <32649344+Mehmet-Yazici@users.noreply.github.com>
2021-02-05 15:48:09 +0300

    stick animation works with motion time

--- SkyGliderClone/Assets/Stick.cs
+++ SkyGliderClone/Assets/Stick.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Stick : MonoBehaviour
 {
     Animator anim;
+    float motionTimeTemp = 0f;
     // Start is called before the first frame update
     private void Awake()
     {
@@ -29,6 +30,26 @@ public class Stick : MonoBehaviour
         {
             anim.SetBool("released", true);
             anim.SetBool("isBending", false);
+            Debug.Log(motionTimeTemp);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            motionTimeTemp += 0.0058f;
+            if (motionTimeTemp > 1.223f)
+            {
+                motionTimeTemp = 1.223f;
+            }
+            anim.SetFloat("motionTime", motionTimeTemp);
+        }
+        else
+        {
+            motionTimeTemp -= 0.026f;
+            if (motionTimeTemp < 0)
+            {
+                motionTimeTemp =0f;
+            }
+            anim.SetFloat("motionTime", motionTimeTemp);
         }
     }
 }

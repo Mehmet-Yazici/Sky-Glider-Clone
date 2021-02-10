0ed2958ab8aec8b16207bc8b78a5f08d4156dd37
Mehmet Yazıcı <32649344+Mehmet-Yazici@users.noreply.github.com>
2021-02-10 18:18:51 +0300

    worked on opening wing position and speed after closing wing

--- SkyGliderClone/Assets/Player.cs
+++ SkyGliderClone/Assets/Player.cs
@@ -14,6 +14,8 @@ public class Player : MonoBehaviour
     bool onStick = true;
     bool EnteredTrigger;
     public bool EnteredTriggerOnce=false;
+    public int counter = 0;
+    int done = 0; //variable to check if addtorque has been used
 
     public void OnTriggerEnter(Collider other)
     {
@@ -57,36 +59,35 @@ public class Player : MonoBehaviour
 
             if (Input.GetMouseButton(0))
             {
+                done = 0;
                 anim.enabled = true;
                 anim.SetBool("isGliding", true);
-                rb.angularVelocity -= new Vector3(4f, 0f, 0f) * Time.deltaTime;
-                Debug.Log(playerTransform.rotation.eulerAngles.x);
-                /*if(rb.angularVelocity.x > 0) { 
-                    rb.angularVelocity -= new Vector3(16f, 0f, 0f ) * Time.deltaTime;
-                    
-                    Debug.Log(playerTransform.rotation.eulerAngles.x);
-                }
-                else if(playerTransform.rotation.x >100 ||(playerTransform.rotation.x <-90f && playerTransform.rotation.x > -180f)){
-                    playerTransform.rotation = Quaternion.RotateTowards(playerTransform.rotation, Quaternion.Euler(-95f, 0f, 0f), 750f * Time.deltaTime);
-                } 
-                else
-                {
-                    rb.angularVelocity = new Vector3(0f, 0f, 0f);
-                    playerTransform.rotation = Quaternion.RotateTowards(playerTransform.rotation, Quaternion.Euler(90f, 0f, 0f), 750f * Time.deltaTime);
-                }*/
+
+                //if(rb.angularVelocity.x > 0) { rb.angularVelocity -= new Vector3(rb.angularVelocity.x / 500f, 0f, 0f) * Time.deltaTime; }
+                rb.angularVelocity = new Vector3(0.01f, 0f, 0f);
+                playerTransform.rotation = Quaternion.RotateTowards(playerTransform.rotation, Quaternion.Euler(90f, 0f, 0f), 300f * Time.deltaTime);
+                
 
             }
             else
             {
                 anim.SetBool("isGliding", false);
-                
+                if (done == 0)
+                {
+                    rb.AddTorque(600f, 0f, 0f, ForceMode.Force);
+                    done = 1;
+                }
             }
 
 
             if (Input.GetMouseButton(1))
             {
-                rb.angularVelocity = new Vector3(0f, 0f, 0f);
-                playerTransform.rotation.eulerAngles.Set(90f, playerTransform.rotation.eulerAngles.y, playerTransform.rotation.eulerAngles.z);
+                //rb.angularVelocity = new Vector3(0f, 0f, 0f);
+                //playerTransform.rotation.eulerAngles.Set(90f, playerTransform.rotation.eulerAngles.y, playerTransform.rotation.eulerAngles.z);
+                //playerTransform.eulerAngles  .Set(90f, playerTransform.eulerAngles.y, playerTransform.eulerAngles.z);
+                Vector3 tmp = playerTransform.eulerAngles;
+                tmp.x = 90f;
+                playerTransform.eulerAngles = tmp;
             }
         }
 
